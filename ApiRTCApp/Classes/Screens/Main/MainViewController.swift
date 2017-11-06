c6da9e70df22ba9c848bee465974371d3e96b0fa
Aleksandr Khorobrykh <aleksandr.khorobrykh@gmail.com>
2017-11-06 16:45:22 +0500

    two ways call UI changes

--- ApiRTCApp/Classes/Screens/Main/MainViewController.swift
+++ ApiRTCApp/Classes/Screens/Main/MainViewController.swift
@@ -29,31 +29,52 @@ class MainViewController: UIViewController {
         remoteView.backgroundColor = .gray
         self.view.addSubview(remoteView)
         
-        ApiRTC.rtc.initialize(localView: localView, remoteView: remoteView)
-        
         textField = UITextField()
-        textField.text = "791098"
+        textField.text = "27543"
         textField.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
         self.view.addSubview(textField)
         textField.snp.makeConstraints { (make) in
-            make.centerY.equalTo(self.view.snp.centerY).offset(0)
+            make.top.equalTo(remoteView.snp.bottom).offset(20)
             make.centerX.equalTo(self.view.snp.centerX)
             make.width.equalTo(250)
             make.height.equalTo(30)
         }
         
-        let button = UIButton()
-        button.backgroundColor = .red
-        button.setTitle("call".loc(), for: .normal)
-        self.view.addSubview(button)
-        button.snp.makeConstraints { (make) in
+        let callButton = UIButton()
+        callButton.backgroundColor = .green
+        callButton.setTitle("call".loc(), for: .normal)
+        self.view.addSubview(callButton)
+        callButton.snp.makeConstraints { (make) in
+            make.width.equalTo(100)
+            make.height.equalTo(30)
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.centerX.equalTo(self.view.snp.centerX).offset(-110)
+        }
+        callButton.addTarget(self, action: #selector(tapCallButton(_:)), for: .touchUpInside)
+        
+        let answerButton = UIButton()
+        answerButton.backgroundColor = .green
+        answerButton.setTitle("answer".loc(), for: .normal)
+        self.view.addSubview(answerButton)
+        answerButton.snp.makeConstraints { (make) in
             make.width.equalTo(100)
             make.height.equalTo(30)
             make.top.equalTo(textField.snp.bottom).offset(20)
             make.centerX.equalTo(self.view.snp.centerX)
         }
-
-        button.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
+        answerButton.addTarget(self, action: #selector(tapAnswerButton(_:)), for: .touchUpInside)
+        
+        let hangupButton = UIButton()
+        hangupButton.backgroundColor = .red
+        hangupButton.setTitle("hangUp".loc(), for: .normal)
+        self.view.addSubview(hangupButton)
+        hangupButton.snp.makeConstraints { (make) in
+            make.width.equalTo(100)
+            make.height.equalTo(30)
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.centerX.equalTo(self.view.snp.centerX).offset(110)
+        }
+        hangupButton.addTarget(self, action: #selector(tapHangUpButton(_:)), for: .touchUpInside)
         
         userIdLabel = UILabel()
         userIdLabel.textAlignment = .center
@@ -61,11 +82,11 @@ class MainViewController: UIViewController {
         userIdLabel.snp.makeConstraints { (make) in
             make.width.equalTo(100)
             make.height.equalTo(30)
-            make.top.equalTo(button.snp.bottom).offset(20)
+            make.top.equalTo(answerButton.snp.bottom).offset(20)
             make.centerX.equalTo(self.view.snp.centerX)
         }
         
-        // todo
+        // FIXME:
         checkPerms { (ok) in
             //print(ok)
         }
@@ -88,16 +109,30 @@ class MainViewController: UIViewController {
             
         }
         ApiRTC.connect()
+        
+        ApiRTC.rtc.initialize()
+        ApiRTC.rtc.add(localView: localView, remoteView: remoteView)
     }
     
-    @objc func tapButton(_ button: UIButton) {
+    @objc func tapCallButton(_ button: UIButton) {
         
         if let caleeId = textField.text, caleeId.count > 0 {
-            ApiRTC.rtc.call(calleeId: caleeId)
+            let call = RTCCall(mediaType: .video, callee: [caleeId])
+            ApiRTC.rtc.start(call: call)
         }
     }
+    
+    @objc func tapAnswerButton(_ button: UIButton) {
+        
+        ApiRTC.rtc.answer()
+    }
+    
+    @objc func tapHangUpButton(_ button: UIButton) {
+        
+        ApiRTC.rtc.hangUp()
+    }
 
-    // todo tmp make sdk checkers
+    // FIXME: tmp make sdk checkers
     func checkPerms(completion: ((_ granted: Bool) -> Void)? = nil) {
         
         if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) ==  AVAuthorizationStatus.authorized {
